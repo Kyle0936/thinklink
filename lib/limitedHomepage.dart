e7761acdee1aa7dacf541e7bb90e7b2543306460
Chentao Wang <cwang556@wisc.edu>
2019-08-02 16:47:44 -0700

    add markers

--- lib/limitedHomepage.dart
+++ lib/limitedHomepage.dart
@@ -1,7 +1,4 @@
-import 'dart:async';
-import 'dart:math';
-import 'dart:ui';
-
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 import './Signup&Login.dart';
@@ -16,40 +13,99 @@ class MainWidget extends StatefulWidget {
 class _MainWidgetState extends State<MainWidget> {
   int _count = 0;
   GoogleMapController mapController;
+  static const LatLng _center = const LatLng(43.073051, -89.401230);
   // Completer<GoogleMapController> _controller = Completer();
 
   var clients = [];
 
   var currentClient;
   var currentBearing;
+  Map<MarkerId, Marker> markers = <MarkerId, Marker>{};
 
-  static final CameraPosition _kGooglePlex = CameraPosition(
-    target: LatLng(43.073051, -89.401230),
-    zoom: 14.4746,
-  );
+  MarkerId selectedMarker;
+  int _markerIdCounter = 1;
+
+  void initState(){
+    super.initState();
+    setState(() {
+      populateClients();
+    });
+  }
+
+  populateClients() {
+    clients = [];
+    Firestore.instance.collection('markers').getDocuments().then((docs) {
+      if (docs.documents.isNotEmpty) {
+        setState(() {
+          // clientsToggle = true;
+        });
+        for (int i = 0; i < docs.documents.length; ++i) {
+          clients.add(docs.documents[i].data);
+          initMarker(docs.documents[i].data);
+        }
+      }
+    });
+  }
+
+   initMarker(client) {
+    // mapController.clearMarkers().then((val) {
+    //   mapController.addMarker(MarkerOptions(
+    //       position:
+    //           LatLng(client['location'].latitude, client['location'].longitude),
+    //       draggable: false,
+    //       infoWindowText: InfoWindowText(client['clientName'], 'Nice')));
+    // });
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+  }
+
+  void _onMarkerTapped(MarkerId markerId) {
+    final Marker tappedMarker = markers[markerId];
+    if (tappedMarker != null) {
+      setState(() {
+        if (markers.containsKey(selectedMarker)) {
+          final Marker resetOld = markers[selectedMarker]
+              .copyWith(iconParam: BitmapDescriptor.defaultMarker);
+          markers[selectedMarker] = resetOld;
+        }
+        selectedMarker = markerId;
+        final Marker newMarker = tappedMarker.copyWith(
+          iconParam: BitmapDescriptor.defaultMarkerWithHue(
+            BitmapDescriptor.hueGreen,
+          ),
+        );
+        markers[markerId] = newMarker;
+      });
+    }
+  }
+
+
 
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('ThinkLink'),
       ),
-      body: new Expanded(
-        // child: Column(
-        //   mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-        //   crossAxisAlignment: CrossAxisAlignment.stretch,
-        //   children: <Widget>[
-            // Center(
-              child: GoogleMap(
-                mapType: MapType.normal,
-                initialCameraPosition: _kGooglePlex,
-                onMapCreated: (GoogleMapController controller) {
-                  // _controller.complete(controller);
-                },
-              ),
-          //   ),
-          // ],
-        ),
-      // ),
+      body: Stack(
+        children: <Widget>[
+          GoogleMap(
+            onMapCreated: _onMapCreated,
+            initialCameraPosition: CameraPosition(
+              target: _center,
+              zoom: 16.0,
+            ),
+            markers: Set<Marker>.of(markers.values),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(16.0),
+            child:null,
+          )
+        ],
+//          _widgetOptions.elementAt(_selectedIndex),
+      ),
       bottomNavigationBar: BottomAppBar(
         child: ButtonBar(
           children: <Widget>[
